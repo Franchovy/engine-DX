24f6f1e15b75fd3541a3f4ccb4fb7100752040e3
Franchovy <franchovy@pm.me>
2025-11-12 19:07:14 +0100

    WIP: Pathfinding graph functional

--- src/modules/LDtkPathFinding.lua
+++ src/modules/LDtkPathFinding.lua
@@ -1,16 +1,27 @@
 local gfx <const> = playdate.graphics
 
+local SOLID_COLLISION_GROUPS <const> = 2 ^ (GROUPS.Solid - 1) | 2 ^ (GROUPS.SolidExceptElevator - 1)
+
 LDTkPathFinding = {}
 
-local levels = {}
-local locations = {}
+---@enum
+PATHFINDING_ADDITIONAL_CONFIGURATIONS = {
+    UpperRight = 1,
+    UpperDoubleRight = 2,
+    DoubleRight = 3,
+    LowerRight = 4,
+    LowerDoubleRight = 5
+}
 
-local function _hasOverlappingWallSprite(x, y, levelBounds)
-    local obstacleSprites = gfx.sprite.querySpritesAtPoint(levelBounds.x + x * 32 + 16,
-        levelBounds.y + y * 32 - 16)
+local levels = {}
+---@type {x: number, y: number, xGraph: number, yGraph: number}[]
+
+local function _hasOverlappingWallSprite(x, y)
+    local obstacleSprites = gfx.sprite.querySpritesAtPoint(x, y)
+
     for _, spriteOverlapping in pairs(obstacleSprites) do
-        -- If sprite overlapping is wall, then return false
-        if spriteOverlapping:getTag() == TAGS.Wall then
+        -- If sprite overlapping is wall, then return
+        if spriteOverlapping:getGroupMask() & SOLID_COLLISION_GROUPS ~= 0 then
             return true
         end
     end
@@ -18,28 +29,22 @@ local function _hasOverlappingWallSprite(x, y, levelBounds)
     return false
 end
 
----comment
----@param graph playdate.pathfinder.graph
----@param widthGraph number
----@param callbackShouldAddConnection fun(xNode1: number, yNode1: number, xNode2: number, yNode2: number, xPositionThrough:number, yPositionThrough:number) : boolean
-local function _addMissingDiagonalConnections(graph, widthGraph, heightGraph, callbackShouldAddConnection)
-    for _, node in pairs(graph:allNodes()) do
-        if node.x > 1 and node.y > 1 and callbackShouldAddConnection(node.x, node.y, node.x - 1, node.y - 1, node.x, node.y - 1) then
-            node:addConnectionToNodeWithXY(node.x - 1, node.y - 1, 14, true)
-        end
+local PIXELS_PER_BLOCK = 32
 
-        if node.x < widthGraph and node.y > 1 and callbackShouldAddConnection(node.x, node.y, node.x + 1, node.y - 1, node.x, node.y - 1) then
-            node:addConnectionToNodeWithXY(node.x + 1, node.y - 1, 14, true)
-        end
+local function _convertLevelToGrid(xLevel, yLevel, bounds)
+    return (xLevel + 16 - bounds.x) / PIXELS_PER_BLOCK, (yLevel + 16 - bounds.y) / PIXELS_PER_BLOCK
+end
 
-        if node.x > 1 and node.y < heightGraph and callbackShouldAddConnection(node.x, node.y, node.x - 1, node.y + 1, node.x - 1, node.y) then
-            node:addConnectionToNodeWithXY(node.x - 1, node.y + 1, 14, true)
-        end
+local function _convertGridToLevel(xGrid, yGrid, bounds)
+    return bounds.x + xGrid * PIXELS_PER_BLOCK - 16, bounds.y + yGrid * PIXELS_PER_BLOCK - 16
+end
 
-        if node.x < widthGraph and node.y < heightGraph and callbackShouldAddConnection(node.x, node.y, node.x + 1, node.y + 1, node.x + 1, node.y) then
-            node:addConnectionToNodeWithXY(node.x + 1, node.y + 1, 14, true)
-        end
-    end
+local function _convertIndexToGrid(index, widthGraph)
+    return (index - 1) % widthGraph + 1, index // widthGraph + 1
+end
+
+local function _convertGridToIndex(x, y, widthGraph)
+    return x + (y - 1) * widthGraph
 end
 
 function LDTkPathFinding.load(levelName)
@@ -69,21 +74,23 @@ function LDTkPathFinding.load(levelName)
             -- Check for overlapping sprites above every x/y tile
 
             for x = xStart, xEnd, 32 do
-                if x == xEnd then
-                    goto continue
-                end
-
+                -- X and Y-Node represent the point where the node is going to go, i.e. above the "Wall" block.
                 local xNode, yNode = x + 16, y - 16
 
-                if _hasOverlappingWallSprite(xNode, yNode, bounds) then
+                if xNode > xEnd or yNode < bounds.y then
                     goto continue
                 end
+
+                if _hasOverlappingWallSprite(xNode, yNode) then
+                    goto continue
+                end
+
+                local xGraph, yGraph = _convertLevelToGrid(xNode, yNode, bounds)
 
                 -- Add node to grid
 
-                table.insert(locations, { x = xNode, y = yNode })
-
-                local index = (xNode - 16 - bounds.x) / 32 + ((yNode + 16 - bounds.y) / 32) * widthGraph
+                local index = _convertGridToIndex(xGraph, yGraph, widthGraph)
+                assert(index > 0 and index <= widthGraph * heightGraph)
                 nodes[index] = 1
 
                 ::continue::
@@ -91,51 +98,143 @@ function LDTkPathFinding.load(levelName)
         end
     end
 
-    level.graph = playdate.pathfinder.graph.new2DGrid(widthGraph, heightGraph, true, nodes)
+    -- FRANCH: Actually disable the diagonal linking just to be safe, until the SDK updates diagonals and we can remove some of the below config checks.
+    level.graph = playdate.pathfinder.graph.new2DGrid(widthGraph, heightGraph, false, nodes)
 
-    -- SDK-bug not adding diagonal connections? - Manually add the diagonal connections in for now.
+    -- Add diagonal and non-direct neighbour connections
 
-    _addMissingDiagonalConnections(level.graph, widthGraph, heightGraph,
-        function(xNode1, yNode1, xNode2, yNode2, xPositionThrough, yPositionThrough)
-            -- Check if through position is free
-            if _hasOverlappingWallSprite(xPositionThrough, yPositionThrough, bounds) then
-                return false
-            end
+    for i, isActive in pairs(nodes) do
+        if isActive == 0 then
+            goto continue
+        end
 
-            -- Check if nodes 1 and 2 position are free
-            if _hasOverlappingWallSprite(xNode1, yNode1, bounds) or _hasOverlappingWallSprite(xNode2, yNode2, bounds) then
-                return false
-            end
+        local configurations = {}
+        local xGraph, yGraph = _convertIndexToGrid(i, widthGraph)
+        local x, y = _convertGridToLevel(xGraph, yGraph, bounds)
 
-            -- Check if nodes 1 and 2 have ground underneath
-            if not _hasOverlappingWallSprite(xNode1, yNode1 + 1, bounds) or not _hasOverlappingWallSprite(xNode2, yNode2 + 1, bounds) then
-                return false
-            end
+        local node = assert(level.graph:nodeWithXY(xGraph, yGraph))
 
-            return true
-        end)
+        -- If direct right neighbor exists, ignore
+        if nodes[_convertGridToIndex(xGraph + 1, yGraph, widthGraph)] == 1 then
+            goto continue
+        end
 
-    _addMissingDiagonalConnections(level.graph, widthGraph, heightGraph, function(xPositionToCheck, yPositionToCheck)
-        return not _hasOverlappingWallSprite(xPositionToCheck, yPositionToCheck, bounds)
-    end)
+        -- Check for upper-right neighbor
 
-    --
+        if
+            xGraph < widthGraph and
+            yGraph > 0 and
+            -- Air with ground (Connection point)
+            nodes[_convertGridToIndex(xGraph + 1, yGraph - 1, widthGraph)] == 1 and
+            -- Space between is clear
+            not _hasOverlappingWallSprite(x, y - 32) then
+            node:addConnectionToNodeWithXY(xGraph + 1,
+                yGraph - 1, 14, true)
+
+            -- No other configurations can work with this.
+            goto continue
+        end
+
+        -- Check for 2x-right neighbor
+
+        if
+            xGraph + 1 < widthGraph and
+            -- Air with ground (Connection point)
+            nodes[_convertGridToIndex(xGraph + 2, yGraph, widthGraph)] == 1 and
+            -- Space between
+            not _hasOverlappingWallSprite(x + 32, y) then
+            node:addConnectionToNodeWithXY(xGraph + 2,
+                yGraph, 20, true)
+
+            configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.DoubleRight] = true
+        end
+
+        -- Check for lower-right neighbor
+
+        if
+            xGraph < widthGraph and
+            yGraph < heightGraph and
+            -- Air with ground (Connection point)
+            nodes[_convertGridToIndex(xGraph + 1, yGraph + 1, widthGraph)] == 1 and
+            -- Space between
+            not _hasOverlappingWallSprite(x + 32, y) then
+            node:addConnectionToNodeWithXY(xGraph + 1,
+                yGraph + 1, 14, true)
+
+            configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.LowerRight] = true
+        end
+
+        -- Check for upper-2x-right neighbor
+
+        if
+            xGraph + 1 < widthGraph and
+            yGraph > 0 and
+            not configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.UpperRight] and
+            -- Air with ground (Connection point)
+            nodes[_convertGridToIndex(xGraph + 2, yGraph - 1, widthGraph)] == 1 and
+            -- Space between
+            not _hasOverlappingWallSprite(x + 32, y) and
+            not _hasOverlappingWallSprite(x + 32, y - 32) then
+            node:addConnectionToNodeWithXY(xGraph + 2,
+                yGraph - 1, 24, true)
+
+            configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.UpperDoubleRight] = true
+        end
+
+        -- Check for lower-2x-right neighbor
+
+        if
+            xGraph + 1 < widthGraph and
+            yGraph < heightGraph and
+            not configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.LowerRight] and
+            not configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.UpperRight] and
+            -- Air with ground (Connection point)
+            nodes[_convertGridToIndex(xGraph + 2, yGraph + 1, widthGraph)] == 1 and
+            -- Space between
+            not _hasOverlappingWallSprite(x + 32, y) and
+            not _hasOverlappingWallSprite(x + 32, y + 32) then
+            node:addConnectionToNodeWithXY(xGraph + 2,
+                yGraph + 1, 24, true)
+
+            configurations[PATHFINDING_ADDITIONAL_CONFIGURATIONS.LowerDoubleRight] = true
+        end
+
+        ::continue::
+    end
 
     levels[levelName] = level
 
-    DebugDrawer.addDebugDrawCall(function()
-        ----[[
-        for _, node in pairs(level.graph:allNodes()) do
-            if #node:connectedNodes() > 1 then
-                gfx.drawCircleAtPoint(bounds.x + (node.x * 32 + 16), bounds.y + (node.y * 32 - 16), 5)
+    if playdate.isSimulator then
+        DebugDrawer.addDebugDrawCall(function()
+            ----[[
+            for i, node in pairs(nodes) do
+                if node == 1 then
+                    local xGrid, yGrid = _convertIndexToGrid(i, widthGraph)
+                    local x, y = _convertGridToLevel(xGrid, yGrid, bounds)
+                    gfx.drawCircleAtPoint(x, y, 2)
+                end
             end
-        end
-        --]]
+            --]]
 
-        ----[[
-        for _, location in pairs(locations) do
-            gfx.drawCircleAtPoint(location.x, location.y, 2)
-        end
-        ----]]
-    end)
+            ----[[
+            for _, node in pairs(level.graph:allNodes()) do
+                if #node:connectedNodes() > 0 then
+                    local x, y = _convertGridToLevel(node.x, node.y, bounds)
+                    gfx.drawCircleAtPoint(x, y, 4)
+
+                    ----[[
+                    for _, connectedNode in pairs(node:connectedNodes()) do
+                        local x2, y2 = _convertGridToLevel(connectedNode.x, connectedNode.y, bounds)
+                        gfx.drawLine(
+                            x,
+                            y,
+                            x2,
+                            y2
+                        )
+                    end
+                end
+            end
+            --]]
+        end)
+    end
 end
